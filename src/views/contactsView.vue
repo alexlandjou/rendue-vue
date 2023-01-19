6fc11bc687cfaa2dbfd6b63c9628cebfe2c6067a
Alex Emmanuel <blakeguccix7@gmail.com>
2023-01-19 10:06:30 +0100

    Vue.js

---
+++ src/views/contactsView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="Personnage">
+        <div class="contacts">
+            <p>0102030405</p>
+            <P>Garix</P>
+       </div>
+       <div>
+        <p>0650809124</p>
+            <P>Alex</P>
+       </div>
+       <div>
+        <p>0758084135</p>
+          <p>Emmanuel</p>
+       </div>
+        
+    </div>
+</template>
---
+++ src/views/journalView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="personnage">
+        <div class="journal">
+            <p>0102030405</p>
+            <button>Garix</button>
+        </div>
+       <div>
+        <p>0650809124</p>
+        <button>Alex</button>
+       </div>
+       <div>
+          <p>0758084135</p>
+          <button>Emmanuel</button>
+       </div>
+
+    </div>
+</template>
